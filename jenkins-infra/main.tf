a9334b0620ce989ec8387e179a1722deb85d7a70
oke2022 <xpdigita@gmail.com>
2025-03-26 12:49:04 -0400

    bug

--- jenkins-infra/main.tf
+++ jenkins-infra/main.tf
@@ -41,7 +41,6 @@ resource "aws_instance" "jenkins_master" {
 	ami           = "ami-084568db4383264d4"  # Ubuntu AMI
 	instance_type = "t2.medium"
   key_name      = "KeyThree.pem"
-  security_groups = [aws_security_group.jenkins_sg.id]
 	tags = {
 	Name = "Jenkins-Master"
 	}
@@ -51,7 +50,6 @@ resource "aws_instance" "jenkins_agent" {
 	ami           = "ami-08b5b3a93ed654d19"  # Amazon Linux AMI
 	instance_type = "t2.micro"
 	key_name      = "KeyThree.pem"
-  security_groups = [aws_security_group.jenkins_sg.id]
 	tags = {
 	Name = "Jenkins-Agent"
 	}
@@ -61,7 +59,6 @@ resource "aws_instance" "mysql_server" {
 	ami           = "ami-0c15e602d3d6c6c4a"  # CentOS AMI
 	instance_type = "t2.micro"
 	key_name      = "KeyThree.pem"
-  security_groups = [aws_security_group.jenkins_sg.id]
 	tags = {
 	Name = "MySQL-Server"
 	}
